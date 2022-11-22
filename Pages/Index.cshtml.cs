c3465a460288e480a3a3de683ebbcbbdd69ba524
Henry Grantham-Smith <116105522+HenryGSmith@users.noreply.github.com>
2022-11-22 01:19:55 +0000

    added basic files for asp.net

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using EtsyInspired.Models;
+using EtsyInspired.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EtsyInspired.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> products { get; private set; }
+
+        public IndexModel(
+            ILogger<IndexModel> logger, 
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService= productService;
+        }
+
+        public void OnGet()
+        {
+            products = ProductService.GetProducts();
+        }
+    }
+}
